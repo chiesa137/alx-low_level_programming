e80444176280a42ee1f8501063a41e57c1c9613f
Chiesa14 <remychiesa14@gmail.com>
2023-03-22 05:41:54 +0200

    all

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 
 /**
-* main - causes an infinite loop
-* Return: 0
-*/
+ * main - causes an infinite loop
+ * Return: 0
+ */
+
+
 
 int main(void)
+
 {
+
 	int i;
 
 	printf("Infinite loop incoming :(\n");
-
 	i = 0;
-
-	while (i < 10)
-	{
-		putchar(i);	/* oops the variable i have to be incremented */
-	}
-	/* to print integer with putchar pass the argument like this i + '0' */
+	/*while (i < 10)*/
+	/*{*/
+	/*	putchar(i);*/
+	/*}*/
 	printf("Infinite loop avoided! \\o/\n");
-
 	return (0);
 }
